0a5ebc92fa44946b1dd2d0b50c589d579186f673
Denic Fedioukovitch <d.fedioukovitch@godeltech.com>
2018-04-28 14:39:28 +0300

    +some code beutify

--- index.php
+++ index.php
@@ -4,65 +4,65 @@
   ini_set('display_startup_errors', 1);
 
   //counting variabels in template wich we should replace
-  function count_variables($template){
-    $varriables_arr= [];
-    $remove_symbols=["%"=>""];
+  function count_variables($template) {
+    $varriablesArr= [];
+    $removeSymbols=["%" => ""];
     foreach ($template as $val) {
-      $split_val = explode(" ",$val);                                                 // spliting our string to words
+      $split_val = explode(" ", $val);                                                 // spliting our string to words
       foreach ($split_val as $str) {
-        if(preg_match('/%\D{1,255}%/',$str)){                                // check if word is template variable
-          array_push($varriables_arr,strtr(substr(trim($str),0,-1),$remove_symbols)); // to get clear variable name;
+        if(preg_match('/%\D{1,255}%/', $str)) {                                // check if word is template variable
+          array_push($varriablesArr, strtr(substr(trim($str), 0, -1), $removeSymbols)); // to get clear variable name;
         };
       }
     }
-    return $varriables_arr;
+    return $varriablesArr;
   }
 
   //replacing variabels in template while this script will be runing on browser
-  function replacing($template, $variable_array){
+  function replacing($template, $variableArray) {
     $result = "";
-    foreach ($template as $tmpl){
-      foreach ($variable_array as $variable){
-        if($variable != "EXECDATE" && $variable != "ENDDATE"){
-          $tmpl=str_replace("%" . $variable . "%",$_POST["$variable"],$tmpl);
+    foreach ($template as $tmpl) {
+      foreach ($variableArray as $variable) {
+        if($variable !== "EXECDATE" && $variable !== "ENDDATE") {
+          $tmpl=str_replace("%" . $variable . "%", $_POST["$variable"], $tmpl);
         }
         else {
-          $run_date = date("d.m.Y H:i:s");
-          $end_date = date_create();
-          date_modify($end_date, $_POST["MONTHNUM"] . "month");
-          $end_date = date_format($end_date, "d.m.Y");
+          $runDate = date("d.m.Y H:i:s");
+          $endDate = date_create();
+          date_modify($endDate, $_POST["MONTHNUM"] . "month");
+          $endDate = date_format($endDate, "d.m.Y");
 
-          $tmpl=str_replace("%EXECDATE%",$run_date,$tmpl);
-          $tmpl=str_replace("%ENDDATE%",$end_date,$tmpl);
+          $tmpl = str_replace("%EXECDATE%", $runDate,$tmpl);
+          $tmpl = str_replace("%ENDDATE%", $endDate,$tmpl);
         }
       }
-    $result=$result .  $tmpl . "<br>";
+    $result = $result .  $tmpl . "<br>";
     }
     return $result;
   }
 
   //replacing variabeles in template while this script will be runing on cli mod
-  function replacing_cli($template, $variable_array,$argv){
+  function replacing_cli($template, $variableArray, $argv) {
     $result = "";
-    $i=1;                                                         // index to get elements from $argv array
-    foreach ($template as $tmpl){
-      foreach ($variable_array as $variable){
-        if($variable != "EXECDATE" && $variable != "ENDDATE"){
-          $tmpl=str_replace("%" . $variable . "%",$argv[$i],$tmpl);
+    $i = 1;                                                         // index to get elements from $argv array
+    foreach ($template as $tmpl) {
+      foreach ($variableArray as $variable) {
+        if($variable !== "EXECDATE" && $variable !== "ENDDATE") {
+          $tmpl = str_replace("%" . $variable . "%", $argv[$i], $tmpl);
           $i++;
         }
         else {
-          $run_date = date("d.m.Y H:i:s");
-          $end_date = date_create();
-          date_modify($end_date, $argv[3] . "month");
-          $end_date = date_format($end_date, "d.m.Y");
+          $runDate = date("d.m.Y H:i:s");
+          $endDate = date_create();
+          date_modify($endDate, $argv[3] . "month");
+          $endDate = date_format($endDate, "d.m.Y");
 
-          $tmpl=str_replace("%EXECDATE%",$run_date,$tmpl);
-          $tmpl=str_replace("%ENDDATE%",$end_date,$tmpl);
+          $tmpl = str_replace("%EXECDATE%", $runDate, $tmpl);
+          $tmpl = str_replace("%ENDDATE%", $endDate, $tmpl);
         }
       }
-    $i=1;
-    $result=$result .  $tmpl;
+    $i = 1;
+    $result = $result .  $tmpl;
     }
     return $result;
   }
@@ -73,19 +73,19 @@
       $tpl = file('template.tpl');
 
       //cheking for erros
-      if(count(count_variables($tpl)) > count($argv)+1){
-        array_push($error,"You missed some arguments");
+      if(count(count_variables($tpl)) > count($argv) + 1) {
+        array_push($error, "You missed some arguments");
       }
-      else if(count(count_variables($tpl)) < count($argv)+1) {
-        array_push($error,"You wrote some extra arguments arguments");
+      else if(count(count_variables($tpl)) < count($argv) + 1) {
+        array_push($error, "You wrote some extra arguments arguments");
       }
 
       if(is_numeric($argv[3]) === false) {
-        array_push($error," 3rd argument should be number");
+        array_push($error, " 3rd argument should be number");
       }
 
       //if there is an erorr
-      if(empty($error) === false){
+      if(empty($error) === false) {
         foreach ($error as $err) {
           echo  $err . "\n";
         }
@@ -93,35 +93,35 @@
       }
       else {                                                      //if there is no any erorr
         // Replacing with neeeded values
-        $text =  replacing_cli($tpl,count_variables($tpl),$argv);
+        $text =  replacing_cli($tpl, count_variables($tpl), $argv);
         echo $text;                                               //to make echo into cli (if script was started from browser echo will be made in massage.php)
 
       }
     }
-    else if($_SERVER['REQUEST_METHOD'] === 'POST'){               //if script was run from browser
+    else if($_SERVER['REQUEST_METHOD'] === 'POST') {               //if script was run from browser
       $tpl = file('template.tpl');
 
       // checking for errors
       foreach (count_variables($tpl) as $value) {
-        if($value == "EXECDATE" || $value == "ENDDATE"){
+        if($value === "EXECDATE" || $value === "ENDDATE") {
           continue;
         }
-        if($value == "MONTHNUM" && is_numeric($_POST["$value"]) === false) {
-          array_push($error,$value ." should be number");
+        if($value === "MONTHNUM" && is_numeric($_POST["$value"]) === false) {
+          array_push($error, $value . " should be number");
           continue;
         }
-        if(empty($_POST["$value"]) === true && $_POST["$value"] !== "0"){
-          array_push($error,"No " . $value ." written!");
+        if(empty($_POST["$value"]) === true && $_POST["$value"] !== "0") {
+          array_push($error, "No " . $value . " written!");
         }
       }
 
       // if there is some erorrs
-      if(empty($error) === false){
+      if(empty($error) === false) {
         return;
       }
       else {                                                      //if no any errors run main part
         // Replacing with neeeded values
-        $text =  replacing($tpl,count_variables($tpl));
+        $text =  replacing($tpl, count_variables($tpl));
       }
     }
     else {                                                        // if index.php runed from browser 1st time with no data in post.
